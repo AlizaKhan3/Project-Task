dda5f394a59a74307d0ff5b9004b789638feec80
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-18 23:41:21 +0500

    Create App.jsx

---
+++ frontend-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import SignupCard from './pages/signup';
+import './App.css';
+import LoginCard from './pages/login';
+// import CardComponent from './components/card';
+
+function App() {
+  return (
+    <div>
+     <LoginCard></LoginCard>
+     <SignupCard></SignupCard>
+    </div>
+  );
+}
+
+export default App;
