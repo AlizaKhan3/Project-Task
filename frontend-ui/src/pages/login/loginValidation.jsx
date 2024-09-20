15ceed29d07fcebebb6ed8567d93e54c412ad4f5
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-21 01:27:48 +0500

    Update loginValidation.jsx

--- frontend-ui/src/pages/login/loginValidation.jsx
+++ frontend-ui/src/pages/login/loginValidation.jsx
@@ -1,30 +1,23 @@
-function validation(values){
-    alert("")
+function validation(values) {
     let error = {};
-    const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/
-    const password_pattern = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
+    const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const password_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,16}$/;
 
-    if(values.email === ""){
-        error.email = "Please enter your email"
-    }
-    else if(!email_pattern.test(values.email)){
-         error.email = "Invalid Email!"
-    }
-    else{
-        error.email = ""
+    if (!values.email) {
+        error.email = "Please enter your email";
+    } else if (!email_pattern.test(values.email)) {
+        error.email = "Invalid Email!";
+    } else {
+        error.email = null;
     }
 
-    if(values.password === ""){
-        error.password = "Please enter your password"
-    }
-    else if(!password_pattern.test(values.password)){
-        error.password = "Invalid Password!"
-    }
-    else{
-        error.password = ""
+    if (!values.password) {
+        error.password = "Please enter your password";
+    } else if (!password_pattern.test(values.password)) {
+        error.password = "Invalid Password!";
+    } else {
+        error.password = null;
     }
     return error;
 }
-
-
 export default validation;
