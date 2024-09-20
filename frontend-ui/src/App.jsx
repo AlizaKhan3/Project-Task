9807b302a9f7124651ff68dbc6d4320b0e9261c4
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-20 23:30:58 +0500

    Update App.jsx

--- frontend-ui/src/App.jsx
+++ frontend-ui/src/App.jsx
@@ -2,13 +2,27 @@ import SignupCard from './pages/signup/signup';
 import './App.css';
 import LoginCard from './pages/login/login';
 // import CardComponent from './components/card';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import AppNotFound from './components/pageNotFound';
 
 function App() {
   return (
-    <div>
-     <LoginCard></LoginCard>
-     <SignupCard></SignupCard>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginCard/>}/>
+      <Route path='/register' element={<SignupCard/>}/>
+      <Route path='/*' element={<AppNotFound/>}/>
+
+    </Routes>
+    </BrowserRouter>
+
+
+    // <div>
+    //  <LoginCard></LoginCard>
+    //  <SignupCard></SignupCard>
+
+
+    // </div>
   );
 }
 
