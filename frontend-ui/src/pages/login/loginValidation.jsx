a67c3b847c24daf3687c2c0510a08fd618779ec9
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-21 00:25:02 +0500

    Update loginValidation.jsx

--- frontend-ui/src/pages/login/loginValidation.jsx
+++ frontend-ui/src/pages/login/loginValidation.jsx
@@ -1,3 +1,30 @@
 function validation(values){
+    alert("")
+    let error = {};
+    const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/
+    const password_pattern = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
 
+    if(values.email === ""){
+        error.email = "Please enter your email"
+    }
+    else if(!email_pattern.test(values.email)){
+         error.email = "Invalid Email!"
+    }
+    else{
+        error.email = ""
+    }
+
+    if(values.password === ""){
+        error.password = "Please enter your password"
+    }
+    else if(!password_pattern.test(values.password)){
+        error.password = "Invalid Password!"
+    }
+    else{
+        error.password = ""
+    }
+    return error;
 }
+
+
+export default validation;
