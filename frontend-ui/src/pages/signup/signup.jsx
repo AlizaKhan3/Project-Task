f4c00b464661982b735d48da46a2fb4bf6373c36
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-23 21:51:58 +0500

    Update signup.jsx

--- frontend-ui/src/pages/signup/signup.jsx
+++ frontend-ui/src/pages/signup/signup.jsx
@@ -1,45 +1,3 @@
-// import * as React from 'react';
-// import CardComponent from '../../components/card';
-// import "./signup.css";
-
-// function SignupCard() {
-//   const fields = [
-//     { label: 'Email', type: 'text' },
-//     { label: 'Password', type: 'password' },
-//     { label: 'Phone Number', type: 'text' },
-//     { label: 'Country', type: 'select' },
-//     { label: 'Location', type: 'text' }, // added location field
-//   ];
-
-//   const countryOptions = [
-//     { label: 'USA', value: 'USA' },
-//     { label: 'Canada', value: 'Canada' },
-//     { label: 'Mexico', value: 'Mexico' },
-//   ];
-
-//   const buttonAction = () => {
-//     console.log('Signup button clicked');
-//   };
-
-//   return (
-//     <div className="card-container">
-//       {/* <div className="card"> */}
-//       <CardComponent
-//         title="Signup"
-//         subtitle="Create an account"
-//         fields={fields}
-//         buttonLabel="Signup"
-//         buttonAction={buttonAction}
-//         countryOptions={countryOptions}
-//       />
-//       {/* </div> */}
-//     </div>
-//   );
-// }
-
-// export default SignupCard;
-
-
 import React, { useState } from 'react';
 import {
   MDBBtn,
@@ -49,91 +7,243 @@ import {
   MDBCard,
   MDBCardBody,
   MDBInput,
-  MDBCheckbox,
-  MDBIcon
-}
-  from 'mdb-react-ui-kit';
-import { Link } from 'react-router-dom';
+  MDBCheckbox
+} from 'mdb-react-ui-kit';
+import { Link, useNavigate } from 'react-router-dom';
 import validation from './signupValidations';
 import { Typography } from 'antd';
+import axios from 'axios';
 
 const { Text } = Typography;
 
-function SignupCard() {
-  const [values, setvalues] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: ''
-  })
+function AppSignupCard() {
+  const [values, setValues] = useState({ firstName: '', lastName: '', email: '', password: '' });
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
 
   const handleInputValue = (e) => {
-    setvalues(prev => ({ ...prev, [e.target.name]: [e.target.value] }))
-  }
-
-  const [errors, setErrors] = useState({});
+    setValues(prev => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setErrors(validation(values));
-  }
+    console.log('handleSubmit function called')
+    const validationErrors = validation(values);
+    setErrors(validationErrors);
+
+    // if (Object.keys(validationErrors).length === 0) { // No validation errors
+    if (!errors.firstName && !errors.lastName && !errors.email && !errors.password) {
+      axios.post('http://localhost:3000/register', values)
+        .then(res => {
+          // After successful signup, navigate to root
+          res.redirect("/");
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
   return (
     <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
       <form onSubmit={handleSubmit}>
-      <MDBRow>
-
-        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
-
-          <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{ color: "000" }}>
-            Landsat Reflectance Data:<br />
-            <span style={{ color: 'hsl(218, 81%, 75%)' }}>On the Fly and at your Fingertips
-            </span>
-          </h1>
-        </MDBCol>
-        <MDBCol md='6' className='position-relative'>
-          <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-          <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
-          <MDBCard className='my-5 bg-glass'>
-            <MDBCardBody className='p-5'>
-              <MDBRow>
-                <MDBCol col='6'>
-                  <MDBInput name='firstName' wrapperClass='mb-4' label='First name' id='form1' type='text' />
-                </MDBCol>
-
-                <MDBCol col='6'>
-                  <MDBInput name='lastName' wrapperClass='mb-4' label='Last name' id='form2' type='text' />
-                </MDBCol>
-              </MDBRow>
-
-              <div style={{ marginBottom: '10px' }}>
+        <MDBRow>
+          <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
+            <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{ color: "#000" }}>
+              Landsat Reflectance Data:<br />
+              <span style={{ color: "#ce2031" }}>On the Fly and at your Fingertips</span>
+            </h1>
+          </MDBCol>
+          <MDBCol md='6' className='position-relative'>
+            <MDBCard className='my-5 bg-glass'>
+              <MDBCardBody className='p-5'>
+                <MDBRow>
+                  <MDBCol col='6' style={{marginBottom: 10}}>
+                    <MDBInput onChange={handleInputValue} name='firstName' wrapperClass='mb-1' label='First name' id='form1' type='text' />
+                    {errors.firstName && <Text type="danger">{errors.firstName}</Text>}
+                  </MDBCol>
+                  <MDBCol col='6' style={{marginBottom: 10}}>
+                    <MDBInput onChange={handleInputValue} name='lastName' wrapperClass='mb-1' label='Last name' id='form2' type='text' />
+                    {errors.firstName && <Text type="danger">{errors.firstName}</Text>}
+                  </MDBCol>
+                </MDBRow>
+                <div style={{marginBottom: 15}}>
                 <MDBInput onChange={handleInputValue} name='email' label='Email' wrapperClass='mb-1' id='form3' type='email' />
-                {errors.email !== null && <Text type="danger">{errors.email}</Text>}
-              </div>
-              <div style={{ marginBottom: '20px' }}>
+                {errors.email && <Text type="danger">{errors.email}</Text>}
+                </div>
+                <div style={{marginBottom: 15}}>
                 <MDBInput onChange={handleInputValue} name='password' wrapperClass='mb-1' label='Password' id='form4' type='password' />
-                {errors.password !== null && <Text type="danger">{errors.password}</Text>}
-              </div>
-
-              <div className='d-flex justify-content-center mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
-              </div>
-
-              <MDBBtn type='submit' className='w-100 mb-4' size='md'>sign up</MDBBtn>
-
-              <div className="text-center">
-                <p>Already have an Account? <Link to="/" style={{ backgroundColor: "transparent", boxShadow: "none", color: "#3b71ca", border: "none" }}> Login</Link></p>
-              </div>
-
-            </MDBCardBody>
-          </MDBCard>
-
-        </MDBCol>
-
-      </MDBRow>
-    </form>
-
-    </MDBContainer >
+                {errors.password && <Text type="danger">{errors.password}</Text>}
+                </div>
+                <MDBCheckbox style={{background: "#ce2031", borderColor:"#ce2031"}} name='flexCheck' id='flexCheckDefault' label='Subscribe to our newsletter' />
+                {/* <MDBBtn type='submit' className='w-100 mb-4' size='md' style={{background: "#ce2031"}}>Sign Up</MDBBtn> */}
+                <MDBBtn onClick={() => console.log('Button clicked')} type='submit' className='w-100 mb-4' size='md'>Sign Up</MDBBtn>
+                <div className="text-center">
+                  <p>Already have an Account? <Link to="/" style={{ backgroundColor: "transparent", boxShadow: "none", color: "#ce2031" }}> Login</Link></p>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </form>
+    </MDBContainer>
   );
 }
 
-export default SignupCard;
+export default AppSignupCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // const handleSubmit = (e) => {
+    // e.preventDefault();
+    // const err = validation(values);
+    // setErrors(err);
+  
+    // if (!err.firstName && !err.lastName && !err.email && !err.password) {
+    //   axios.post('http://localhost:3000/register', values)
+    //   .then((res) => {
+    //     console.log(' Axios request successful:', res);
+    //     if (res.status === 200) {
+    //       console.log('Navigating to login page...');
+    //       navigate('/');
+    //     } else {
+    //       console.error('Error registering user:', res.status);
+    //       alert('Error registering user:', res.status);
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     alert('Error registering user:', err.message);
+    //   });
+    // }
+
+
+
+      // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const err = validation(values);
+  //   setErrors(err);
+
+  //   if (!errors.firstName && !errors.lastName && !errors.email && !errors.password) {
+  //     axios.post('http://localhost:3000/register', values)
+  //       .then(res => {
+  //         navigate('/');
+  //       })
+  //       .catch(err => console.log(err));
+  //   }
+  // };
+
+
+
+
+
+
+
+
+
+
+
+
+   // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const err = validation(values);
+  //   setErrors(err);
+  
+  //   if (Object.keys(err).length === 0) {
+  //     axios.post('http://localhost:3000/register', values)
+  //       .then((res) => {
+  //         navigate('/');
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //         alert('Error registering user: ' + err.message);
+  //       });
+  //   } else{
+  //       console.log('Axios request successful:');
+  //   if (axios.status === 200) {
+  //     navigate('/');
+  //   } 
+  //   }
+  // };
