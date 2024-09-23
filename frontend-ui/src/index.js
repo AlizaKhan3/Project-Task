e0d510d9f5a3b8da56962949b439d07be7ceb5f8
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-23 21:52:11 +0500

    Update index.js

--- frontend-ui/src/index.js
+++ frontend-ui/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-// import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
 
@@ -13,5 +12,11 @@ root.render(
   </React.StrictMode>
 );
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 reportWebVitals();
