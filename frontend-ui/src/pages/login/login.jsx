48705ac9d71e2e202ef6e22cc07114f1530b6d7c
Afia <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-26 02:09:30 +0500

    Update login.jsx

--- frontend-ui/src/pages/login/login.jsx
+++ frontend-ui/src/pages/login/login.jsx
@@ -197,7 +197,7 @@
 // // }
 
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   MDBBtn,
   MDBContainer,
@@ -211,55 +211,64 @@ import {
   from 'mdb-react-ui-kit';
 import validation from './loginValidation';
 import { Typography } from 'antd';
+import axios from 'axios';
 
 const { Text } = Typography;
 
 function AppLoginCard() {
-  console.log('Rendering LoginCard component');
-  const [values, setvalues] = useState({
-    email: '',
-    password: ''
-  })
-
-  const handleInputValue = (e) => {
-    setvalues(prev => ({ ...prev, [e.target.name]: e.target.value }))
-  }
-
+  const [values, setValues] = useState({ email: '', password: '' });
   const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors(validation(values));
-    console.log(values);
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('handleSubmit function called');
+    const validationErrors = validation(values);
+    console.log('validationErrors:', validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      // No errors, proceed with API call
+      axios.post('http://localhost:8000/', values)
+        .then((res) => {
+          console.log("response-->", res);
+          if (res.data.statusbar === "Successfully inserted data") {
+            // navigate('/');
+            console.log("login form working")
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      // Errors present, update errors state
+      setErrors(validationErrors);
+      console.log("Errors present, not submitting data");
+    }
+  };
+ 
 
   return (
     <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
       <form onSubmit={handleSubmit}>
         <MDBRow>
-
           <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
-
              <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{ color: "black" }}>
               Landsat Reflectance Data:<br />
               <span style={{ color: '#ce2031' }}>On the Fly and at your Fingertips
               </span>
             </h1>
           </MDBCol>
-
           <MDBCol md='6' className='position-relative'>
-
             <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
             <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
-
             <MDBCard className='my-5 bg-glass'>
               <MDBCardBody className='p-5'>
                 <div style={{ marginBottom: '10px' }}>
-                  <MDBInput onChange={handleInputValue} name='email' label='Email' wrapperClass='mb-1' id='form3' type='email' />
+                  {/* <MDBInput onChange={handleInputValue} name='email' label='Email' wrapperClass='mb-1' id='form3' type='email' /> */}
+                  <MDBInput onChange={e => setValues({ ...values, email: e.target.value })} name='email' wrapperClass='mb-1' label='Email' id='form1' type='email' />
+
                   {errors.email !== null && <Text type="danger">{errors.email}</Text>}
                 </div>
                 <div style={{ marginBottom: '20px' }}>
-                  <MDBInput onChange={handleInputValue} name='password' wrapperClass='mb-1' label='Password' id='form4' type='password' />
+                  <MDBInput onChange={e => setValues({...values, password: e.target.value })} name='password' wrapperClass='mb-1' label='Password' id='form4' type='password' />
                   {errors.password !== null && <Text type="danger">{errors.password}</Text>}
                 </div>
                 {/* <div className='d-flex justify-content-center mb-4'>
