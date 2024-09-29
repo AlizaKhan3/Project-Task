f79bcf4075fa59c06d246f130e3e50e3037b5118
Afia <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-29 22:47:07 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,33 +12,62 @@ const cors = require('cors');
 // import bcrypt from 'bcrypt';
 // import cookieParser from 'cookie-parser';
 const salt = 10;
-
+// const saltRounds = 10;
 const app = express();
 const PORT = 8000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
 app.use(cookieParser());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "userdata"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "userdata"
 })
 
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Signup API');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the Signup API');
+// });
+
+// app.post('/register', (req, res) => {
+//   const sql = "INSERT INTO signup (`firstname`, `lastname`, `email`, `password`) VALUES (?)";
+//   bcrypt.genSalt(10, (err, salt) => {
+//     if (err) {
+//       return res.json({ Error: "Error generating salt" });
+//     }
+//     bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+//       if (err) {
+//         return res.json({ Error: "Error hashing password" });
+//       }
+//       console.log("Hashed Password: ", hash);
+//       const values = [
+//         req.body.firstname,
+//         req.body.lastname,
+//         req.body.email,
+//         hash
+//       ]
+//       db.query(sql, [values], (err, data) => {
+//         if (err) return res.json({ Error: "Here Error inserting values" });
+//         return res.json({ statusbar: "Successfully inserted data" });
+//       })
+//   });
+// });
+// });
 
 app.post('/register', (req, res) => {
   const sql = "INSERT INTO signup (`firstname`, `lastname`, `email`, `password`) VALUES (?)";
   bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
-    if(err){
-      return res.json({
-        Error: "Error hashing password"
-      })
-    };
+    if (err) {
+      return res.json({ Error: "Error hashing password" });
+    }
+    console.log("Hashed Password: ", hash);
     const values = [
       req.body.firstname,
       req.body.lastname,
@@ -46,78 +75,77 @@ app.post('/register', (req, res) => {
       hash
     ]
     db.query(sql, [values], (err, data) => {
-      if (err) return res.json({Error: "Error inserting values"});
-      return res.json({statusbar: "Successfully inserted data"});
-  })
-  //   // Store user data in database
-    // const userId = data.insertId;
-    // const user = { id: userId, ...values };
-    // db.query("INSERT INTO users SET ?", user, (err, data) => {
-    //   if (err) {
-    //     return res.json("Error");
-    //   }
-    //   res.json({ message: "User registered successfully" });
-    // });
+      if (err) return res.json({ Error: "Here Error inserting values" });
+      return res.json({ statusbar: "Successfully inserted data" });
+    })
+
   });
 });
 
-app.post('/', (req, res)) 
-
+app.post('/', (req, res) => {
+  const sql = 'SELECT * FROM signup WHERE email = ?';  //cannot compare password as it is hashed
+  db.query(sql, [req.body.email], (err, data) => {
+    if (err) return res.json({ Error: "Login error!" });
+    if (data.length > 0) {
+      bcrypt.compare(req.body.password.toString().trim(), data[0].password, (err, response) => {
+        if (err) return res.json({ Error: "Password Not Compared" });
+        if (response) {
+          const name = data[0].name;
+          const token = jwt.sign({name}, "jwt-secret-key", {expiresIn: '1d'});
+          res.cookie('token', token );
+          return res.json({ statusbar: "Success Login"});
+        }
+        else {
+          return res.json({ Error: "Password not matched!" })
+        }
+      })
+    } else {
+      return res.json({ Error: "EMAIL NOT MATCHED/Existed!" });
+    }
+  })
+})
 
 app.listen(PORT, () => {
-    console.log("listening on port 8000");
+  console.log("listening on port 8000");
 });
 
 
-// const express = require('express');
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
-// const userModel = require('./models/user');
 
-// const app = express();
+// app.post('/', (req, res) => {
+//   const sql = 'SELECT * FROM signup WHERE email = ?';
+//   db.query(sql, [req.body.email], (err, data) => {
+//     if (err) return res.json({ Error: "Login error!" });
+//     if (data.length > 0) {
+//       const storedHash = data[0].password;
+//       const inputPassword = req.body.password.toString().trim(); // Trim the input password
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+//       console.log("Stored hash:", storedHash); // Log the stored hash
+//       console.log("Input password:", inputPassword); // Log the input password
 
-// app.post('/login', async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await userModel.findOne({ email });
-//   if (!user) {
-//     return res.status(401).send('Email or password is incorrect');
-//   }
-//   const isValid = await bcrypt.compare(password, user.password);
-//   if (!isValid) {
-//     return res.status(401).send('Email or password is incorrect');
-//   }
-//   const token = jwt.sign({ email: user.email, userid: user._id }, process.env.SECRETKEY);
-//   res.json(token);
-// });
-
-// app.post('/createuser', async (req, res) => {
-//   const { name, email, password } = req.body;
-//   const user = new userModel({ name, email, password });
-//   await user.save();
-//   const token = jwt.sign({ email: user.email, userid: user._id }, process.env.SECRETKEY);
-//   res.json(token);
-// });
-
-// app.listen(8000, () => console.log('Server started on port 8000'));
-
-
-
-
-// app.post('/register', (req, res) => {
-//     const sql = "INSERT INTO signup (`firstname`, `lastname`, `email`, `password`) VALUES (?)";
-//     const values = [
-//         req.body.firstname,
-//         req.body.lastname,
-//         req.body.email,
-//         req.body.password
-//     ];
-//     db.query(sql, [values], (err, data) => {
-//         if (err) {
-//             return res.json("Error");
+//       bcrypt.compare(inputPassword, storedHash, (err, response) => {
+//         if (err) return res.json({ Error: "Password Not Compared" });
+//         if (response) {
+//           return res.json({ statusbar: "Password matched successfully from the database" });
+//         } else {
+//           return res.json({ Error: "Password not matched!" });
 //         }
-//         return res.json(data);
-//     });
+//       });
+//     } else {
+//       return res.json({ Error: "EMAIL NOT MATCHED!" });
+//     }
+//   });
+// });
+
+
+
+
+
+//   // Store user data in databases
+// const userId = data.insertId;
+// const user = { id: userId, ...values };
+// db.query("INSERT INTO users SET ?", user, (err, data) => {
+//   if (err) {
+//     return res.json("Error");
+//   }
+//   res.json({ message: "User registered successfully" });
 // });
